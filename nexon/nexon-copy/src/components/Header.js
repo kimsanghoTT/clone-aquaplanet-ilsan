a5f9ea46bc56611f83f9b3d611ae6e3293ba56b5
kimsangho <qwert8494@naver.com>
2025-05-01 09:26:42 +0900

    헤더

---
+++ nexon/nexon-copy/src/components/Header.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "../css/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-left">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-list"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+          />
+        </svg>
+        <p>메뉴</p>
+      </div>
+
+      <div className="header-logo">
+        <a href="/">
+          <img src="/nexon/logo_nexon.png" alt="logo" />
+        </a>
+      </div>
+
+      <div className="header-right">
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-credit-card"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z" />
+            <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
+          </svg>
+          <p className="header-toggle-text">넥슨현대카드</p>
+        </span>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-geo-alt-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
+          </svg>
+          <p className="header-toggle-text">PC방 찾기</p>
+        </span>
+        <span>
+          <p>회원가입</p>
+        </span>
+        <span>
+          <button>로그인</button>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Header;
