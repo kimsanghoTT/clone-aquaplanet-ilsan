85c9dc5ebd903b8fe77dac10d265a586f411ac17
kimsangho <qwert8494@naver.com>
2025-05-19 15:31:51 +0900

    헤더+메인페이지 동영상

--- practice/src/components/main/main_video_section.js
+++ practice/src/components/main/main_video_section.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "../../css/main.css";
 
 const MainVideoSection = () => {
   return (
-    <div>
-        <video src="/img/ilsan_main_video.mp4" autoPlay muted loop type="video/mp4" />
+    <div className="video-section-container">
+      <video
+        src="/img/ilsan_main_video.mp4"
+        autoPlay
+        muted
+        loop
+        type="video/mp4"
+      />
+      <div className="main-video-overlay-text">
+      <p>국내 유일 바다코끼리 보유</p>
+      <p>수도권 최대 규모 아쿠아리움</p>
+      <p>아쿠아플라넷 일산</p>
+      </div>
     </div>
   );
 };
