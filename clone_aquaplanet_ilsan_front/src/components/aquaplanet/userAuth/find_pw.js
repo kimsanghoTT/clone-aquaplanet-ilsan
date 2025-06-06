f26fcefdbf5a067c61c3253d42d82c0539453d9f
김상호 <165999006+kimsanghoTT@users.noreply.github.com>
2025-06-06 10:52:51 +0900

    Update find_pw.js

--- clone_aquaplanet_ilsan_front/src/components/aquaplanet/userAuth/find_pw.js
+++ clone_aquaplanet_ilsan_front/src/components/aquaplanet/userAuth/find_pw.js
@@ -124,7 +124,7 @@ const FindPwModal = ({ onClose }) => {
   return (
     <>
       <div className="modal-overlay-film"></div>
-      <div className="find-modal">
+      <div className="modal-container">
         <button type="button" onClick={onClose}>X</button>
         {step === 1 && (
           <div className="search-form">
