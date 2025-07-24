5a29c5c5efee79fac83a3c6d4bf02c74406cc9d9
kimsangho <qwert8494@naver.com>
2025-07-24 09:32:47 +0900

    일부 수정

--- clone_aquaplanet_ilsan_front/src/components/ilsan/friends/sub_components/friends_swiper.jsx
+++ clone_aquaplanet_ilsan_front/src/components/ilsan/friends/sub_components/friends_swiper.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Scrollbar } from "swiper/modules";
 import "swiper/css";
